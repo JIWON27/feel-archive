9eaac48e09d670d3bc90197ceb18fc5cff3811bf
minjiwon <mjo4260@naver.com>
2025-12-12 12:26:57 +0900

    chore: TokenProperties 설정을 위해 어노테이션 추가

--- rest-api/src/main/java/com/feeleats/api/RestApiApplication.java
+++ rest-api/src/main/java/com/feeleats/api/RestApiApplication.java
@@ -1,11 +1,13 @@
 package com.feeleats.api;
 
+import com.feeleats.api.auth.infra.jwt.TokenProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TokenProperties.class)
 public class RestApiApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(RestApiApplication.class, args);
   }
