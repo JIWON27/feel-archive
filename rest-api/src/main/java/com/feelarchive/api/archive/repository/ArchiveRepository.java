54d285a2e9f664393694854c0aec80ad844ff315
minjiwon <mjo4260@naver.com>
2025-12-26 15:55:51 +0900

    feat: 아카이브 조회를 위한 Repository 메소드 정의

--- rest-api/src/main/java/com/feelarchive/api/archive/repository/ArchiveRepository.java
+++ rest-api/src/main/java/com/feelarchive/api/archive/repository/ArchiveRepository.java
@@ -1,10 +1,13 @@
 package com.feelarchive.api.archive.repository;
 
 import com.feelarchive.api.archive.domain.Archive;
+import com.feelarchive.api.user.domain.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArchiveRepository extends JpaRepository<Archive, Long> {
 
+  List<Archive> findByUser(User user);
 }
