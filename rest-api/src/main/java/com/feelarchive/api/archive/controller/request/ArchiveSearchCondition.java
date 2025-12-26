47aa7e1ea8f992c0395e8a322e11ab2900a1b39f
minjiwon <mjo4260@naver.com>
2025-12-26 22:16:49 +0900

    feat: 검색 조건 DTO에 userId 추가

--- rest-api/src/main/java/com/feelarchive/api/archive/controller/request/ArchiveSearchCondition.java
+++ rest-api/src/main/java/com/feelarchive/api/archive/controller/request/ArchiveSearchCondition.java
@@ -3,12 +3,16 @@ package com.feelarchive.api.archive.controller.request;
 import com.feelarchive.api.emotion.domain.Emotion;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ArchiveSearchCondition {
+  @Setter
+  Long userId;
   Emotion emotion;
   String keyword;
   ArchiveSortType sortType;
+
 }
